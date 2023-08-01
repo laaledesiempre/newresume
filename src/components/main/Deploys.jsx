ac1ea9c47f0d957516ca7850382d18056f9f1425
laaledesiempre <alejandrabotacura@outlook.com.ar>
2023-08-01 00:02:34 -0300

    added new pictures

--- src/components/main/Deploys.jsx
+++ src/components/main/Deploys.jsx
@@ -10,22 +10,22 @@ export const Deploys = () => {
       link: "https://alesfccphrasegenerator.netlify.app/",
     }, {
       name: "PoMoDoRo",
-      image: "https://github.com/laaledesiempre/newresume/blob/reactVersion/public/Captura%20desde%202023-07-29%2000-13-06.png?raw=true",
+      image: "https://github.com/laaledesiempre/newresume/blob/reactVersion/public/Captura%20desde%202023-07-31%2023-55-01.png?raw=true",
       imageResponsive: "https://github.com/laaledesiempre/newresume/blob/reactVersion/public/Captura%20desde%202023-07-31%2023-34-59.png?raw=true",
       link: "https://laaledesiempre.github.io/FCCPoMoDoRo/",
     }, {
       name: "DrumPad",
-      image: "https://github.com/laaledesiempre/newresume/blob/reactVersion/public/Captura%20desde%202023-07-29%2000-13-06.png?raw=true",
+      image: "https://github.com/laaledesiempre/newresume/blob/reactVersion/public/Captura%20desde%202023-07-31%2023-55-07.png?raw=true",
       imageResponsive: "https://github.com/laaledesiempre/newresume/blob/reactVersion/public/Captura%20desde%202023-07-31%2023-35-19.png?raw=true",
       link: "https://laaledesiempre.github.io/FCCdrumbmachine/",
     }, {
       name: "Markdown Parser",
-      image: "https://github.com/laaledesiempre/newresume/blob/reactVersion/public/Captura%20desde%202023-07-29%2000-13-06.png?raw=true",
+      image: "https://github.com/laaledesiempre/newresume/blob/reactVersion/public/Captura%20desde%202023-07-29%2000-11-37.png?raw=true",
       imageResponsive: "https://github.com/laaledesiempre/newresume/blob/reactVersion/public/Captura%20desde%202023-07-31%2023-37-13.png?raw=true",
       link: "https://laaledesiempre.github.io/markdown/",
     }, {
       name: "Practical work for UTN diplomacy",
-      image: "https://github.com/laaledesiempre/newresume/blob/reactVersion/public/Captura%20desde%202023-07-29%2000-13-06.png?raw=true",
+      image: "https://github.com/laaledesiempre/newresume/blob/reactVersion/public/Captura%20desde%202023-07-29%2000-11-33.png?raw=true",
       imageResponsive: "https://github.com/laaledesiempre/newresume/blob/reactVersion/public/Captura%20desde%202023-07-31%2023-36-10.png?raw=true",
       link: "https://laaledesiempre.github.io/Trabajo_UTN_Front/",
     },
