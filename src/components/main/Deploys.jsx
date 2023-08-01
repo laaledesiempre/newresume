643ebc75591c0d0c973da71b6c3fc91eaf093219
laaledesiempre <alejandrabotacura@outlook.com.ar>
2023-07-31 23:57:07 -0300

    solved proflems and fixed pictures

--- src/components/main/Deploys.jsx
+++ src/components/main/Deploys.jsx
@@ -1,31 +1,32 @@
 import React from 'react'
+import { VscCode } from "react-icons/vsc";
 import './deploys.css'
 export const Deploys = () => {
   const deploys = [
     {
       name: "Quote Generator",
       image: "https://github.com/laaledesiempre/newresume/blob/reactVersion/public/Captura%20desde%202023-07-29%2000-13-06.png?raw=true",
-      imageResponsive: "https://github.com/laaledesiempre/newresume/blob/reactVersion/public/Captura%20desde%202023-07-29%2000-13-06.png?raw=true",
+      imageResponsive: "https://github.com/laaledesiempre/newresume/blob/reactVersion/public/Captura%20desde%202023-07-29%2001-09-47.png?raw=true",
       link: "https://alesfccphrasegenerator.netlify.app/",
     }, {
       name: "PoMoDoRo",
       image: "https://github.com/laaledesiempre/newresume/blob/reactVersion/public/Captura%20desde%202023-07-29%2000-13-06.png?raw=true",
-      imageResponsive: "https://github.com/laaledesiempre/newresume/blob/reactVersion/public/Captura%20desde%202023-07-29%2000-13-06.png?raw=true",
+      imageResponsive: "https://github.com/laaledesiempre/newresume/blob/reactVersion/public/Captura%20desde%202023-07-31%2023-34-59.png?raw=true",
       link: "https://laaledesiempre.github.io/FCCPoMoDoRo/",
     }, {
       name: "DrumPad",
       image: "https://github.com/laaledesiempre/newresume/blob/reactVersion/public/Captura%20desde%202023-07-29%2000-13-06.png?raw=true",
-      imageResponsive: "https://github.com/laaledesiempre/newresume/blob/reactVersion/public/Captura%20desde%202023-07-29%2000-13-06.png?raw=true",
+      imageResponsive: "https://github.com/laaledesiempre/newresume/blob/reactVersion/public/Captura%20desde%202023-07-31%2023-35-19.png?raw=true",
       link: "https://laaledesiempre.github.io/FCCdrumbmachine/",
     }, {
       name: "Markdown Parser",
       image: "https://github.com/laaledesiempre/newresume/blob/reactVersion/public/Captura%20desde%202023-07-29%2000-13-06.png?raw=true",
-      imageResponsive: "https://github.com/laaledesiempre/newresume/blob/reactVersion/public/Captura%20desde%202023-07-29%2000-13-06.png?raw=true",
+      imageResponsive: "https://github.com/laaledesiempre/newresume/blob/reactVersion/public/Captura%20desde%202023-07-31%2023-37-13.png?raw=true",
       link: "https://laaledesiempre.github.io/markdown/",
     }, {
       name: "Practical work for UTN diplomacy",
       image: "https://github.com/laaledesiempre/newresume/blob/reactVersion/public/Captura%20desde%202023-07-29%2000-13-06.png?raw=true",
-      imageResponsive: "https://github.com/laaledesiempre/newresume/blob/reactVersion/public/Captura%20desde%202023-07-29%2000-13-06.png?raw=true",
+      imageResponsive: "https://github.com/laaledesiempre/newresume/blob/reactVersion/public/Captura%20desde%202023-07-31%2023-36-10.png?raw=true",
       link: "https://laaledesiempre.github.io/Trabajo_UTN_Front/",
     },
   ]
@@ -39,7 +40,7 @@ export const Deploys = () => {
           <img className="mobile-image" src={e.imageResponsive} />
           {/* todo: make an responsive screenshot for mobile version*/}
           <a target="_blank" href={e.link}>Click to see!</a>
-          <br />
+          <span>~<VscCode />~</span>
         </article>
       })}
     </section>
